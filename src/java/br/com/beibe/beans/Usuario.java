6c1540a6c74aa54717c6fb46b3eafb63f7006a2a
RafaelKulka <rafaelkulka9879@gmail.com>
2022-04-13 21:56:20 -0300

    Adição de beans

---
+++ src/java/br/com/beibe/beans/Usuario.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.beibe.beans;
+
+/**
+ *
+ * @author Rafael Kulka
+ */
+public class Usuario {
+    private String nome;
+    private String CPF;
+    private String email;
+    private String endereco;
+    private String telefone;
+    private String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
