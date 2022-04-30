c197829c13af3ad9545ab0c876b8e2a74a435804
RafaelKulka <rafaelkulka9879@gmail.com>
2022-04-30 12:42:57 -0300

    -

---
+++ src/java/br/com/beibe/dao/AtendimentoDAO.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.beibe.dao;
+
+import br.com.beibe.beans.Atendimento;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+import java.sql.SQLException;
+import br.com.beibe.exception.DAOException;
+/**
+ *
+ * @author Rafael Kulka
+ */
+public class AtendimentoDAO {
+    private static final String QUERY_INSERIR_ATENDIMENTO = "INSERT INTO BEIBE.Atendimento (CreateDate, Cliente, Situacao, Produto, TipoAtendimento, Descricao, Solucao) VALUES (NOW(),?,?,?, ? ,?,?)";
+    //private static final String QUERY_BUSCAR = "SELECT * FROM BEIBE.usuario WHERE email = ? AND senha = CAST(HASH('MD5', ?) AS VARCHAR)";
+    private Connection con = null;
+    
+     public AtendimentoDAO(Connection con) throws DAOException {
+        if (con == null) {
+            throw new DAOException("Conexão nula ao criar AtendimentoDAO.");
+        }
+        this.con = con;
+    }
+    
+    public void inserir(Atendimento a) throws DAOException {
+        try (PreparedStatement st = con.prepareStatement(QUERY_INSERIR_ATENDIMENTO)) {
+            st.setString(1, a.getCliente().getCPF());
+            st.setString(2, "Aberto");
+            st.setString(3, a.getProduto().getNome());
+            st.setString(4, a.getTipoAtendimento().getNome());
+            st.setString(5, a.getDescricao());
+            st.setString(6, a.getSolucao());
+            
+            st.executeUpdate();
+             //throw new DAOException(st.toString());
+
+        } catch(org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException e){
+            throw new DAOException("Atendimento  já cadastrado");
+        }
+        catch (SQLException e) {
+            throw new DAOException( e.toString());
+        }
+
+    }
+
+   /* public Atendimento buscarUser(String email, String senha) {
+        
+        try (PreparedStatement st = con.prepareStatement(QUERY_BUSCAR)) {
+            st.setString(1, email);
+            st.setString(2, senha);
+
+            ResultSet rs = st.executeQuery();
+
+            if (rs.next()) {
+                
+                Usuario user = new Usuario(
+                        rs.getString("nome"),
+                        rs.getString("cpf"),
+                        email,
+                        rs.getString("telefone"),
+                        rs.getInt("tipo"),
+                        rs.getString("rua"),
+                        rs.getString("numero"),
+                        rs.getString("complemento"),
+                        rs.getString("bairro"),
+                        rs.getString("cep"),
+                        rs.getString("cidade"),
+                        rs.getString("estado")
+                );
+                
+                return user;
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            return null;
+        }
+
+    }*/
+
+    
+}
---
+++ src/java/br/com/beibe/model/AtendimentoOperacoes.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.beibe.model;
+
+
+import br.com.beibe.beans.Atendimento;
+import br.com.beibe.dao.AtendimentoDAO;
+import br.com.beibe.dao.ConnectionFactory;
+import br.com.beibe.exception.DAOException;
+
+/**
+ *
+ * @author Rafael Kulka
+ */
+public class AtendimentoOperacoes {
+    public static Atendimento InserirAtendimento(Atendimento atendimento) throws DAOException  {
+        try (ConnectionFactory factory = new ConnectionFactory()) {
+            AtendimentoDAO dao = new AtendimentoDAO(factory.getConnection());
+            dao.inserir(atendimento);
+            return atendimento;
+        } catch (Exception ex) {
+            throw new DAOException("Não foi possivel inserir atendimento. " + ex);
+        }
+    }
+}
